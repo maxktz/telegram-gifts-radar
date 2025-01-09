656907e0b6caa5679efe9877c0be366ba774f566
maxktz <maxktz@icloud.com>
2025-01-09 18:45:52 +0200

    update CD scripts

--- scripts/cd.sh
+++ scripts/cd.sh
@@ -1,5 +1,5 @@
 docker build -t telegram-gifts-radar .
 
-docker run --rm -v "./.env:/app/.env" telegram-gifts-radar bun run migrate
+docker run --rm -v "./sessions:/app/sessions" --env-file ./.env telegram-gifts-radar bun run migrate
 
 docker compose down && docker compose up -d
--- scripts/login.sh
+++ scripts/login.sh
@@ -1,3 +1,1 @@
-source .env
-
 docker run --rm -it -v "./sessions:/app/sessions" --env-file ./.env telegram-gifts-radar bun run start
