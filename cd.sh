d42ecfb89e6213c9955e87ef991b3e287ff78811
maxktz <maxktz@icloud.com>
2025-01-09 05:36:35 +0200

    update migrate script usage

--- cd.sh
+++ cd.sh
@@ -2,6 +2,6 @@ source .env
 
 docker build -t telegram-gifts-radar .
 
-docker run --rm telegram-gifts-radar bunx prisma migrate deploy
+docker run --rm telegram-gifts-radar bun run migrate
 
 docker compose down && docker compose up -d
